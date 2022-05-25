9734a43b961cacb284b070ab31bd82074cbd66ce
JonaDeNeve <jona.deneve@student.hogent.be>
2022-05-26 00:29:25 +0200

    setUp

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
+import './i18n';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
