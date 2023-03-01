4653087201fb4621e0c592f907f1c471f63f695c
JonaDeNeve <jona.deneve@student.hogent.be>
2023-03-01 01:32:46 +0100

    1

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,28 +1,52 @@
 
-const NavBar = () => {
+interface NavBarProps  { 
+    children?: React.ReactNode 
+}
+
+const NavBar = ({ children } : NavBarProps) => {
     return (
         <>
-            <nav className="navbar bg-base-200">
-                <div className="flex-1">
-                    <a className="btn btn-ghost normal-case text-xl">Jona De Neve</a>
+        <div className="drawer">
+            <input id="my-drawer-3" type="checkbox" className="drawer-toggle" /> 
+            <div className="drawer-content flex flex-col">
+                <div className="flex flex-col">
+                    <a className="text-center normal-case text-xl bg-base-300">Jona De Neve</a>
+                    <div className="w-full navbar bg-base-200">
+                        <div className="navbar-start ">
+                            <div className="flex-none lg:hidden">
+                                <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                                </label>
+                            </div>
+                        </div>
+                        <div className="navbar-center">
+                            <div className="flex-none hidden lg:block">
+                                <ul className="menu menu-horizontal">
+                                    <li><a>Home</a></li>
+                                    <li><a>Experience</a></li>
+                                    <li><a>Education</a></li>
+                                    <li><a>Skills</a></li>
+                                    <li><a>Interests</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="navbar-end">
+                        </div>
+                    </div>
                 </div>
-                <div className="flex-none">
-                    <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li tabIndex={0}>
-                        <a>
-                        Parent
-                        <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-                        </a>
-                        <ul className="p-2 bg-base-200">
-                        <li><a>Submenu 1</a></li>
-                        <li><a>Submenu 2</a></li>
-                        </ul>
-                    </li>
-                    <li><a>Item 3</a></li>
-                    </ul>
-                </div>
-            </nav>
+                {children}
+            </div> 
+            <div className="drawer-side">
+                <label htmlFor="my-drawer-3" className="drawer-overlay"></label> 
+                <ul className="menu p-4 w-80 bg-base-100">
+                    <li><a>Home</a></li>
+                    <li><a>Experience</a></li>
+                    <li><a>Education</a></li>
+                    <li><a>Skills</a></li>
+                    <li><a>Interests</a></li>
+                </ul>
+            </div>
+        </div>
         </>
     );
 };
