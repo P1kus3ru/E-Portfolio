53ed211f7ef4f42bd3b8aea65c1386639ea3a688
JonaDeNeve <jona.deneve@student.hogent.be>
2023-03-11 00:29:42 +0100

    1

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,10 @@
 // @ts-check
 /** @type {import("next").NextConfig} */
-
 const config = {
-  basePath: '/E-Portfolio',
-  assetPrefix: `https://P1kus3ru.github.io`,
-  images: {
-    unoptimized: true,
-  },
-  exportPathMap: async function () {
-    return {
-      '/': { page: '/' },
-    }
-  },
   reactStrictMode: true,
-  // i18n: {
-  //   locales: ["en"],
-  //   defaultLocale: "en",
-  // },
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
 };
-
 export default config;
